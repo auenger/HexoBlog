636fa3c714391f719675e18ca0d9cd8b4e2923bd
yzw40459 <yzw40459@ly.com>
2019-05-08 17:03:10 +0800

    Site updated: 2019-05-08 17:03:08

---
+++ miaomiao/js/data.js
@@ -0,0 +1,248 @@
+var data = [
+    {
+        "name": "八方招财",
+        "value": "1",
+        "part": "套装",
+        "formula": [
+            {
+                "name": "生如夏花",
+                "part": "花纹",
+                "formula": [
+                    {
+                        "name": "阿尔卑斯",
+                        "part": "花纹",
+                        "formula": []
+                    },
+                    {
+                        "name": "草莓饼干",
+                        "part": "花纹",
+                        "formula": []
+                    }
+                ]
+            },
+            {
+                "name": "海蓝宝石",
+                "part": "眼睛",
+                "formula": [
+                    {
+                        "name": "夜猫眼",
+                        "part": "眼睛",
+                        "formula": []
+                    },
+                    {
+                        "name": "眯眯眼",
+                        "part": "眼睛",
+                        "formula": []
+                    }
+                ]
+            },
+            {
+                "name": "小绒球",
+                "part": "尾巴",
+                "formula": []
+            },
+            {
+                "name": "偷笑",
+                "part": "嘴巴",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "超能蒙面",
+        "value": "2",
+        "part": "肚皮",
+        "formula": [
+            {
+                "name": "钻石甜心",
+                "part": "肚皮",
+                "formula": []
+            },
+            {
+                "name": "小猫妙盗",
+                "part": "肚皮",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "煤堆探索",
+        "value": "2",
+        "part": "肚皮",
+        "formula": [
+            {
+                "name": "肌肉战士",
+                "part": "肚皮",
+                "formula": []
+            },
+            {
+                "name": "太空战士",
+                "part": "肚皮",
+                "formula": [
+                    {
+                        "name": "超能蒙面",
+                        "part": "肚皮",
+                        "formula": []
+                    },
+                    {
+                        "name": "超能蒙面",
+                        "part": "肚皮",
+                        "formula": []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "海蓝宝石",
+        "value": "3",
+        "part": "眼睛",
+        "formula": [
+            {
+                "name": "夜猫眼",
+                "part": "眼睛",
+                "formula": []
+            },
+            {
+                "name": "眯眯眼",
+                "part": "眼睛",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "童话萌猫",
+        "value": "3",
+        "part": "眼睛",
+        "formula": [
+            {
+                "name": "街头霸喵",
+                "part": "眼睛",
+                "formula": []
+            },
+            {
+                "name": "死鱼眼",
+                "part": "眼睛",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "好怕怕",
+        "value": "3",
+        "part": "眼睛",
+        "formula": [
+            {
+                "name": "怒发冲冠",
+                "part": "眼睛",
+                "formula": []
+            },
+            {
+                "name": "呆萌宝宝",
+                "part": "眼睛",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "小可怜",
+        "value": "3",
+        "part": "眼睛",
+        "formula": [
+            {
+                "name": "确认眼神",
+                "part": "眼睛",
+                "formula": []
+            },
+            {
+                "name": "闭目养神",
+                "part": "眼睛",
+                "formula": [
+                    {
+                        "name": "呆萌宝宝",
+                        "part": "眼睛",
+                        "formula": []
+                    },
+                    {
+                        "name": "专心致志",
+                        "part": "眼睛",
+                        "formula": []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "洋葱面具",
+        "value": "4",
+        "part": "花纹",
+        "formula": [
+            {
+                "name": "虎斑",
+                "part": "花纹",
+                "formula": []
+            },
+            {
+                "name": "美人尖",
+                "part": "花纹",
+                "formula": []
+            }
+        ]
+    },
+    {
+        "name": "爱的魔法",
+        "value": "4",
+        "part": "花纹",
+        "formula": [
+            {
+                "name": "雨花台",
+                "part": "花纹",
+                "formula": []
+            },
+            {
+                "name": "力量印记",
+                "part": "花纹",
+                "formula": [
+                    {
+                        "name": "西部世界",
+                        "part": "花纹",
+                        "formula": []
+                    },
+                    {
+                        "name": "阿尔卑斯",
+                        "part": "花纹",
+                        "formula": []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "傻乎乎",
+        "value": "5",
+        "part": "嘴巴",
+        "formula": [
+            {
+                "name": "开心派",
+                "part": "嘴巴",
+                "formula": [
+                    {
+                        "name": "一阵沉默",
+                        "part": "嘴巴",
+                        "formula": []
+                    },
+                    {
+                        "name": "愤怒",
+                        "part": "嘴巴",
+                        "formula": []
+                    }
+                ]
+            },
+            {
+                "name": "咬牙切齿",
+                "part": "嘴巴",
+                "formula": []
+            }
+        ]
+    }
+];
